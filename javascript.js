4f495675697bb0c5709c0cb4f701c5ce4d0e0dd7
Kaushal0528 <39668328+Kaushal0528@users.noreply.github.com>
2018-06-03 14:23:05 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,163 @@
+// a = 10
+// var a
+// // console.log(a)
+
+// // var variable = 123
+
+// // for(let abc =1; abc<100;abc++){
+// //     console.log(abc)
+
+// // }
+
+// //console.log(functionDif)
+// //var functionDif = function(){
+//     //console.log('some function')
+// //}
+// //functionDef()
+
+// var someObject ={
+//     name:'new car',
+//     engineType:'petrol',
+//     year:1998
+// }
+//  for(runner in someObject){
+//      console.log(runner)
+//  }
+// for(runner in someObject){
+//     console.log(someObject[runner], runner)
+// }
+
+
+
+// function sum(a,b){
+//     console.log(a,b)
+// }
+// sum(1,2) 
+// sum(1,2,3,4,5)
+
+//=====================recvursive=================
+// var globalRunner=0
+// function recur (){
+//     ++globalRunner
+//     console.log(globalRunner)
+//     recur()
+// }
+// recur()
+
+// var globalRunner=0
+// function recur (){
+//     ++globalRunner
+//     if(globalRunner == 90){
+//         return
+//     }
+//     else{
+//         console.log(globalRunner)
+//         recur()
+//     }
+// }
+
+// recur()
+
+
+// var globalRunner=0
+// function recur (endCondition){
+//     ++globalRunner
+//     if(globalRunner == endCondition+1){
+//         return
+//     }
+//     else{
+//         console.log(globalRunner)
+//         recur(endCondition)
+//     }
+// }
+
+// recur(10)
+
+
+
+// function recur (startCondition,endCondition){
+//     ++startCondition
+//     if(startCondition == endCondition+1){
+//         return
+//     }
+//     else{
+//         console.log(startCondition)
+//         recur(startCondition,endCondition)
+//     }
+// }
+
+// recur(5,10)
+
+
+// function recur (startCondition,endCondition){
+//     --startCondition
+//     if(startCondition == endCondition+1){
+//         return
+//     }
+//     else{
+//         console.log(startCondition)
+//         recur(startCondition,endCondition)
+//     }
+// }
+
+// recur(10,0)
+
+// function recur (startCondition,endCondition){
+//     --startCondition
+//     if(startCondition == endCondition+1){
+//         return
+//     }
+//     else{
+//         console.log(startCondition)
+//         recur(startCondition,endCondition)
+//     }
+// }
+
+// recur(10,0)
+
+
+// function addSquares(a,b){
+//     function square(x){
+//         return x * x;
+//     }
+//     return square(a) + square(b);
+// }
+// console.log( addSquares(2,5))
+
+//================string concat method===============
+// var first = 'kaushal'
+// var last = 'singh'
+// var nameOne = first.concat(last)
+// console.log(nameOne)
+
+
+// console.log(Number.Max_Value)
+
+// console.log(Math.floor(Math.random(10)*100))
+// console.log(Math.ceil(Math.random(10)*100))
+
+// var dataObjectName = new Date();
+// console.log(dataObjectName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
